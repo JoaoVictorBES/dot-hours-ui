ffc1baee586e9a7f46c2656e5d4f29869acf6958
Joao Victor <victorsrodrigues99@gmail.com>
2025-02-07 08:49:47 -0300

    Estruturando diretórios e adicionando componentes

---
+++ src/app/Components/lancamento-horas/lancamento-horas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-horas',
+  standalone: true,
+  imports: [],
+  templateUrl: './lancamento-horas.component.html',
+  styleUrl: './lancamento-horas.component.scss'
+})
+export class LancamentoHorasComponent {
+
+}
